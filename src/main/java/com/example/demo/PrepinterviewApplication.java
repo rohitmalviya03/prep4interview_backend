21c6d202e17748d807ec01d29aa2f5716b9464c2
rohitmalviya03 <rmrohit439@gmail.com>
2024-11-24 22:28:33 +0530

    first commit

---
+++ src/main/java/com/example/demo/PrepinterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepinterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepinterviewApplication.class, args);
+	}
+
+}
